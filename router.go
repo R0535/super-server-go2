647e0a6556cf85fe4421b9cb0a8408ee5d748253
R0535 <jghr053@gmail.com>
2021-05-12 01:09:14 -0500

    Server

---
+++ router.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Router struct {
+	rules map[string]http.HandlerFunc
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+func NewRouter() *Router {
+	return &Router{
+		rules: make(map[string]http.HandlerFunc),
+	}
+}
+
+func(r *Router) FindHandler(path string)(http.Handler, bool) {
+	handler,exist := r.rules[path]
+	return handler,exist
+}
