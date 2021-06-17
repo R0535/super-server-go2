f6f538d0a497da338a020621c3404cc1635faf00
R0535 <jghr053@gmail.com>
2021-06-16 19:05:52 -0500

    aaaaa

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	server := NewServer(":3000")
 	var x int32
+	fmt.Printf("%d", &x)
 	server.Handle("GET", "/", HandleRoot)
 	server.Handle("GET", "/api", HandleApi)
 	server.Handle("POST", "/create", PostRequest)
