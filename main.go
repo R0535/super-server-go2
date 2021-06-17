770fb4c055d09242bb660df8c4f23d051292247c
R0535 <jghr053@gmail.com>
2021-06-16 19:05:35 -0500

    updatedd stuff

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
 	server := NewServer(":3000")
-
+	var x int32
 	server.Handle("GET", "/", HandleRoot)
 	server.Handle("GET", "/api", HandleApi)
 	server.Handle("POST", "/create", PostRequest)
